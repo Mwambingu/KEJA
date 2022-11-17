6db72a64b4dff00bcd383cac50ccf96bb20e3153
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-11-17 20:09:45 +0300

    Generating author n creating dirs

---
+++ scripts/gen_auth.sh
@@ -0,0 +1,8 @@
+{
+		cat <<- 'EOH'
+		# This file lists all individuals having contributed content to the repository.
+		# For how it is generated, see `hack/generate-authors.sh`.
+	EOH
+		echo
+			git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+		} > AUTHORS
