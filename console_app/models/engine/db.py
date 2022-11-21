018e2a84c1a282465a4a8956f46e1083855e3233
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-11-21 15:26:44 +0300

    Setting up DB functions

--- console_app/models/engine/db.py
+++ console_app/models/engine/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.basemodel import BaseModel
+from models.basemodel import BaseModel, Base
 
 class DBStorage():
   __engine=None
@@ -8,3 +8,19 @@ class DBStorage():
 
   def __init__(self):
     self.__engine = create_engine("mysql+mysqldb://keja_admin:keja001@localhost/Keja_TestDB")
+
+  
+  def new(self, obj):
+    self.__session.add(obj)
+
+  def save(self, obj):
+    self.__session.commit()
+  
+  def delete(self, obj=None):
+    if obj:
+      self.__session.delete(obj)
+
+  def reload(self):
+    Base.metadata.create_all(self.__engine)
+    session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+    self.__session=scoped_session(session_factory)
