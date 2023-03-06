75bbe7f27307c6bbcfe3e86416ba2ff42509d5e1
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-06 23:31:53 +0300

    Updating views routes and js template

--- flask_jinja_app/website/views.py
+++ flask_jinja_app/website/views.py
@@ -6,3 +6,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def index():
     return render_template("dashboard.html")
+
+@views.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@views.route('/houses')
+def house():
+    return render_template("houses.html")
+
+@views.route('/messages')
+def message():
+    return render_template("messages.html")
+
+@views.route('/payments')
+def payment():
+    return render_template("payments.html")
+
+@views.route('/tenants')
+def tenant():
+    return render_template("tenants.html")
