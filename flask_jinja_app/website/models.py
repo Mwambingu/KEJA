cd3d304249398ac117af983be0a10ebf46827d21
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-12 12:11:52 +0300

    Setting db-models in db-modeling branch

--- flask_jinja_app/website/models.py
+++ flask_jinja_app/website/models.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+import datetime
+from uuid import uuid4
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Base(db.Model):
+    id = db.Column(db.String(64), primary_key=True)
+    created_at = db.Column(db.Datetime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.Datetime, default=datetime.datetime.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        self.id = __class__.__name__ + str(uuid4())
