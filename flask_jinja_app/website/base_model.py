6b1d7fc2722449f8caacaf2072358caa3f51eccb
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-13 18:31:56 +0300

    Fixing BaseModel's flask_db inheritance errors

--- flask_jinja_app/website/base_model.py
+++ flask_jinja_app/website/base_model.py
@@ -2,10 +2,10 @@ import datetime
 from uuid import uuid4
 from . import db
 
-class Base(db.Model):
+class BaseModel():
     id = db.Column(db.String(64), primary_key=True)
-    created_at = db.Column(db.Datetime, default=datetime.datetime.utcnow)
-    updated_at = db.Column(db.Datetime, default=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         self.id = __class__.__name__ + str(uuid4())
--- flask_jinja_app/website/models.py
+++ flask_jinja_app/website/models.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 from . import db
-from base_model import Base
+from .base_model import BaseModel
 
-class Landlord(Base, db.Model):
+class Landlord(BaseModel, db.Model):
     __tablename__ = "landlords"
     first_name = db.Column(db.String(16), nullable=False)
     last_name = db.Column(db.String(16), nullable=False)
     email = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
 
-class House(Base, db.Model):
+class House(BaseModel, db.Model):
     __tablename__ = "houses"
     house_name = db.Column(db.String(32), nullable=False)
     landlord_id = db.Column(db.String(64), db.ForeignKey('landlords.id'), nullable=False)
     number_of_apartments =  db.Column(db.Integer, default=0, nullable=True)
 
-class Apartment(Base, db.Model):
+class Apartment(BaseModel, db.Model):
     __tablename__ = "apartments"
     apartment_no = db.Column(db.String(32), nullable=False)
     room_type = db.Column(db.String(32), nullable=False)
     rent = db.Column(db.Integer, nullable=False)
     house_id = db.Column(db.String(64), db.ForeignKey("houses.id"), nullable=False)
 
-class Tenant(Base, db.Model):
+class Tenant(BaseModel, db.Model):
     __tablename__ = "tenants"
     first_name = db.Column(db.String(16), nullable=False)
     last_name = db.Column(db.String(16), nullable=False)
