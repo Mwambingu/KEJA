6185ae4b5d2055fc3106eb3579dabfdcfa509b9c
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-13 19:20:31 +0300

    Pep 8

--- flask_jinja_app/website/__init__.py
+++ flask_jinja_app/website/__init__.py
@@ -4,11 +4,22 @@ from flask import Flask
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+DB_USER = "keja_flask_user"
+DB_PASS = "kejaflask001"
+
 
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'dev'
-  app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(DB_NAME)
-  db.init_app(app)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://{}:{}@localhost/{}".format(
+        DB_USER, DB_PASS, DB_NAME)
+    db.init_app(app)
 
-  return app
+    from website.models import Landlord, Tenant, House, Apartment
+    create_database(app)
+
+    return app
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
