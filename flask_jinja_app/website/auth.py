51f4fe90a565544440c4672ba06152a6713ac8cd
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-21 20:42:31 +0300

    Fixing login, signup routes and template styling

--- flask_jinja_app/website/auth.py
+++ flask_jinja_app/website/auth.py
@@ -4,11 +4,11 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/auth/login')
+@auth.route('/login')
 def login():
     return render_template("login.html")
 
 
-@auth.route('/auth/signup')
+@auth.route('/signup')
 def signup():
     return render_template("signup.html")
