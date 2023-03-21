779f2294abac7bef60673c0e52c2148ba7817817
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-03-21 17:56:58 +0300

    Adding routes to auth.py

--- flask_jinja_app/main.py
+++ flask_jinja_app/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from website import create_app
 from website.views import views
-
+from website.auth import auth
 app = create_app()
 app.register_blueprint(views)
+app.register_blueprint(auth)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask_jinja_app/website/auth.py
+++ flask_jinja_app/website/auth.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/auth/login')
+def login():
+    return "<h1>Hello Login</h1>"
+
+
+@auth.route('/auth/signup')
+def signup():
+    return "<h1>Hello Signup</h1>"
